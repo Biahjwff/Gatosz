6815726e0b7d3cc4208186b1cac44e0008acf3f4
Ana Beatriz <anabeatrizkraener@gmail.com>
2025-11-25 21:49:35 -0300

    Design: instalando a biblioteca fontawesome para icones, criando a pasta fonts e colocando elas lá, criando o app.blade e a pasta landingPage

---
+++ resources/views/landingPage/index.blade.php
@@ -0,0 +1,24 @@
+@extends('app')
+@section('body')
+    {{-- Header --}}
+    <div class="bg-rosa-claro py-1 flex justify-between items-center">
+
+        <div class="chilanka bg-white rounded-tr-2xl rounded-br-2xl px-2 py-1">
+            <span>Gatosz</span>
+        </div>
+
+        <div class="flex gap-5">
+            <button type="button" class="bg-rosa-escuro text-white rounded-lg px-2 py-1 shadow-sm"> Sobre</button>
+
+            <button type="button" class="bg-rosa-escuro text-white rounded-lg px-2 py-1 shadow-sm"> Sobre</button>
+
+            <button type="button" class="bg-rosa-escuro text-white rounded-lg px-2 py-1 shadow-sm"> Sobre</button>
+
+            <button type="button" class="bg-rosa-escuro text-white rounded-lg px-2 py-1 shadow-sm"> Sobre</button>
+        </div>
+
+        <div class="bg-rosa-escuro rounded-tl-2xl rounded-bl-2xl px-2 py-1">
+            <i class="fa-regular fa-user">oi</i>
+        </div>
+    </div>
+@endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,16 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            colors: {
+                "rosa-claro": "#FC8871",
+                "rosa-escuro": "#F1715E",
             },
         },
     },
