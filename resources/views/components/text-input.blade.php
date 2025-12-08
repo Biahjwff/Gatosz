bf5983d6287f861fa36eee474254482bcd7fa6e2
Ana Beatriz <anabeatrizkraener@gmail.com>
2025-12-08 14:25:24 -0300

    Design: alertas de erro no crud com o idioma português agora

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'h-8 w-80 bg-bege-escuro rounded-lg border-none shadow-sm shadow-marrom-escuro text-lg placeholder-marrom-escuro transition duration-400 focus:ring-1 focus:ring-marrom-escuro']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'w-full h-8 w-80 bg-bege-escuro rounded-lg border-none shadow-sm shadow-marrom-escuro text-lg placeholder-marrom-escuro transition duration-400 focus:ring-1 focus:ring-marrom-escuro']) }}>
