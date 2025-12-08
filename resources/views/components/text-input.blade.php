caf642e2b1b983fe3e8dc15110a92e77f1a42474
Ana Beatriz <anabeatrizkraener@gmail.com>
2025-12-07 23:54:50 -0300

    Design: login estilizado

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'h-8 w-80 bg-bege-escuro rounded-lg border-none shadow-sm shadow-marrom-escuro text-lg placeholder-marrom-escuro focus:ring-0']) }}>
