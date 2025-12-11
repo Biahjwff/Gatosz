e7e6736d94eb9ef8f3b951e940d83d97686a93b2
Ana Beatriz <anabeatrizkraener@gmail.com>
2025-12-10 22:41:20 -0300

    Design: login do cliente estilizado

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gatosz</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/landingPage.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/landingPage.js', 'resources/js/crud.js'])
 
 </head>
 
